62b33189ba3445931a1dacace6ec930922fd5dba
John White <cihatakv@gmail.com>
2020-05-24 01:21:47 -0500

    Day6, utilities and pojos added

---
+++ src/test/java/com/automation/tests/day6/POJOPracticeWithORDS.java
@@ -0,0 +1,35 @@
+package com.automation.tests.day6;
+
+import com.automation.pojos.Employee;
+import com.automation.pojos.Link;
+import com.automation.utilities.ConfigurationReader;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import io.restassured.response.Response;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.Matchers.*;
+
+public class POJOPracticeWithORDS {
+
+    @BeforeAll
+    public static void setup(){
+        baseURI = ConfigurationReader.getProperty("ORDS.URI"); // "http://54.224.118.38:1000/ords/hr";
+    }
+
+    @Test
+    public void getEmployeeTest(){
+        Response response = get("/employees/{id}", 100);
+        Employee employee = response.as(Employee.class);
+        System.out.println("employee = " + employee);
+
+        List<Link> links = employee.getLinks();
+        System.out.println("links = " + links);
+    }
+
+
+}
